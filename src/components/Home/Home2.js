8e4cb1746ccdbe39ecceca6dee21d4d23cb8642d
Syed Umar Husain <73102373+syedumarhusain@users.noreply.github.com>
2024-06-29 18:54:33 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/i_umarhusain/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
